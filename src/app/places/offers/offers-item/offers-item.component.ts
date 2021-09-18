d0509210599042331f32f447c3406d72bfad7f8d
Darren Nienaber <staysignal@outlook.com>
2021-09-18 14:11:16 +0200

    updates to general styling

---
+++ src/app/places/offers/offers-item/offers-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Place } from 'src/app/services/places.service';
+
+@Component({
+  selector: 'app-offers-item',
+  templateUrl: './offers-item.component.html',
+  styleUrls: ['./offers-item.component.scss'],
+})
+export class OffersItemComponent implements OnInit {
+
+  @Input() offer: Place;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  getDummyDate() {
+    return new Date();
+  }
+
+}
