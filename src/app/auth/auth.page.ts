75211fd726175c5ebb23c64f3abf2360ac8e0ad0
Darren Nienaber <staysignal@outlook.com>
2021-09-19 10:25:20 +0200

    updates to auth login and signup

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
@@ -10,6 +11,7 @@ import { AuthService } from '../services/auth.service';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -39,8 +41,27 @@ export class AuthPage implements OnInit {
 
         }, 1500);
       })
+  }
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    form.resetForm();
 
+    if (this.isLogin) {
+      // send request to login servers
+    }
+    else {
+      //send request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
