0627525f5952f26e1af7ad9e1023478be8f0665d
Darren Nienaber <staysignal@outlook.com>
2021-09-19 12:59:55 +0200

    updates to auth forms and add offer forms

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -7,6 +7,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
 
